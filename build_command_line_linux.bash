a7395cce2baa9c053dfd59f294eddcf3579cfe27
Joey Payne <jyapayne@gmail.com>
2016-02-08 13:28:21 -0700

    Fixed linux build from being too large.

--- build_command_line_linux.bash
+++ build_command_line_linux.bash
@@ -4,13 +4,13 @@ rm -rf ElectrifyLinux-CMD
 
 VERSION=$(cat files/version.txt)
 
-pyinstaller --onefile --hidden-import PIL.Jpeg2KImagePlugin --hidden-import pkg_resources --hidden-import PIL._imaging --hidden-import configobj --distpath ElectrifyLinux-CMD -n electrify-linux command_line.py
+pyinstaller --onefile --exclude-module PyQt5 --exclude-module PyQt4 --hidden-import PIL.Jpeg2KImagePlugin --hidden-import pkg_resources --hidden-import PIL._imaging --hidden-import configobj --distpath ElectrifyLinux-CMD -n electrify-linux command_line.py
 cp -rf files ElectrifyLinux-CMD/files
 rm -rf ElectrifyLinux-CMD/files/downloads/*
 rm ElectrifyLinux-CMD/files/error.log ElectrifyLinux-CMD/files/last_project_path.txt ElectrifyLinux-CMD/files/recent_files.txt ElectrifyLinux-CMD/files/compressors/upx-mac ElectrifyLinux-CMD/files/compressors/upx-win.exe
 
 rm -rf ElectrifyLinux
-pyinstaller -F --hidden-import PIL.Jpeg2KImagePlugin --hidden-import configobj --hidden-import PIL._imaging --hidden-import pkg_resources -n electrify --distpath ElectrifyLinux main.py
+pyinstaller -F --exclude-module PyQt5 --exclude-module PyQt4 --hidden-import PIL.Jpeg2KImagePlugin --hidden-import configobj --hidden-import PIL._imaging --hidden-import pkg_resources -n electrify --distpath ElectrifyLinux main.py
 cp -rf files ElectrifyLinux/files
 rm -rf ElectrifyLinux/files/downloads/*
 rm ElectrifyLinux/files/error.log ElectrifyLinux/files/last_project_path.txt ElectrifyLinux/files/recent_files.txt ElectrifyLinux/files/compressors/upx-mac ElectrifyLinux/files/compressors/upx-win.exe
